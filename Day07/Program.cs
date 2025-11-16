e36f28a87401540e5ad87d4e8a6d09c95f957ea0
Simon Da Vall <simon.davall@gmail.com>
2025-11-16 20:12:46 +0000

    Day07

--- Day07/Program.cs
+++ Day07/Program.cs
@@ -11,10 +11,12 @@ internal static class Program
 
   public static int Main(string[] args)
   {
+    Console.WriteLine("\n## Day 7: Amplification Circuit ##");
+    Console.WriteLine("https://adventofcode.com/2019/day/7");
+ 
     long resultPartOne = -1;
     long resultPartTwo = -1;
 
-    PrintTitle();
     foreach (var filePath in args)
     {
       Console.WriteLine($"\nFile: {filePath}\n");
@@ -132,13 +134,6 @@ internal static class Program
                       combos.Add([i, j, k, l, m]);
                     }
     return combos;
-  }
-
-  private static void PrintTitle()
-  {
-    Console.WriteLine("# Advent of Code 2019 #");
-    Console.WriteLine("## Day 7: Amplification Circuit ##");
-    Console.WriteLine("https://adventofcode.com/2019/day/7");
   }
 
   private static void PrintResult(string partNo, string result, Stopwatch sw)
