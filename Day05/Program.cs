738f6926d20e980f4c4e79dc3144e298dbf6fbc3
Simon Da Vall <simon.davall@gmail.com>
2025-11-20 22:07:10 +0000

    Day05

--- Day05/Program.cs
+++ Day05/Program.cs
@@ -1,37 +1,16 @@
-﻿using System.Diagnostics;
-using AocHelper;
+﻿using AocHelper;
 using Spacecraft;
 
 namespace Day05;
 
-internal static class Program {
-  private const long ExpectedPartOne = 10987514;
+internal static partial class Program {
+  private const string Title = "\n## Day 5: Sunny with a Chance of Asteroids ##";
+  private const string AdventOfCode = "https://adventofcode.com/2019/day/5";
+
+ private const long ExpectedPartOne = 10987514;
   private const long ExpectedPartTwo = 14195011;
 
-  public static int Main(string[] args) {
-    Console.WriteLine("\n## Day 5: Sunny with a Chance of Asteroids ##");
-    Console.WriteLine("https://adventofcode.com/2019/day/5");
-
-    long resultPartOne = -1;
-    long resultPartTwo = -1;
-
-    foreach (var filePath in args) {
-      Console.WriteLine($"\nFile: {filePath}\n");
-      long[] program = GetData(filePath);
-      var stopwatch = Stopwatch.StartNew();
-
-      resultPartOne = PartOne(program);
-      PrintResult("1", resultPartOne.ToString(), stopwatch);
-
-      resultPartTwo = PartTwo(program);
-      PrintResult("2", resultPartTwo.ToString(), stopwatch);
-    }
-
-    Console.WriteLine();
-    return resultPartOne == ExpectedPartOne && resultPartTwo == ExpectedPartTwo ? 0 : 1;
-  }
-
-  private static long PartOne(long[] program) {
+ private static long PartOne(long[] program) {
     var computer = new IntcodeComputer(program);
     while (!computer.IsHalted) {
       if (computer.IsAwaitingInput) {
@@ -55,21 +34,5 @@ internal static class Program {
     }
 
     return computer.GetLastOutput;
-  }
-
-  private static long[] GetData(string filePath) {
-    using var streamReader = new StreamReader(filePath);
-    long[] data = streamReader
-      .ReadToEnd()
-      .Split([',', '\n'], StringSplitOptions.RemoveEmptyEntries)
-      .ToLongArray();
-
-    return data;
-  }
-
-  private static void PrintResult(string partNo, string result, Stopwatch sw) {
-    sw.Stop();
-    Console.WriteLine($"Part {partNo} Result: {result} in {sw.Elapsed.TotalMilliseconds}ms");
-    sw.Restart();
   }
 }
