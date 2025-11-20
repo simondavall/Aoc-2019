e58df8f8ee899a934a44db7aa37fd796bd24cceb
Simon Da Vall <simon.davall@gmail.com>
2025-11-20 16:03:00 +0000

    Day12

---
+++ Day12/Program.cs
@@ -0,0 +1,214 @@
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace Day12;
+
+internal static partial class Program {
+  private const long ExpectedPartOne = 7471;
+  private const long ExpectedPartTwo = 376243355967784;
+
+  public static int Main(string[] args) {
+    Console.WriteLine("\n## Day 12: The N-Body Problem ##");
+    Console.WriteLine("https://adventofcode.com/2019/day/12");
+
+    long resultPartOne = -1;
+    long resultPartTwo = -1;
+
+    foreach (var filePath in args) {
+      Console.WriteLine($"\nFile: {filePath}\n");
+      Moon[] input = GetData(filePath);
+      var stopwatch = Stopwatch.StartNew();
+
+      resultPartOne = PartOne(input);
+      PrintResult("1", resultPartOne.ToString(), stopwatch);
+
+      resultPartTwo = PartTwo(input);
+      PrintResult("2", resultPartTwo.ToString(), stopwatch);
+    }
+
+    return resultPartOne == ExpectedPartOne && resultPartTwo == ExpectedPartTwo ? 0 : 1;
+  }
+
+  // This holds the number of steps to take for each file imported.
+  private static Queue<int> _totalSteps = new Queue<int>([10, 100, 1000]);
+
+  private static long PartOne(Moon[] moons) {
+    var step = 0;
+    var totalSteps = _totalSteps.Dequeue();
+    while (step++ < totalSteps) {
+      List<Moon> newMoons = [];
+      foreach (var moon in moons) {
+        newMoons.Add(moon.Copy());
+      }
+      // update velocities
+      for (var i = 0; i < moons.Length; i++) {
+        for (var j = 0; j < moons.Length; j++) {
+          if (i == j) continue;
+          newMoons[i].velocity.dx -= moons[i].position.x.CompareTo(moons[j].position.x);
+          newMoons[i].velocity.dy -= moons[i].position.y.CompareTo(moons[j].position.y);
+          newMoons[i].velocity.dz -= moons[i].position.z.CompareTo(moons[j].position.z);
+        }
+      }
+      // update postions
+      foreach (var moon in newMoons) {
+        moon.position.x += moon.velocity.dx;
+        moon.position.y += moon.velocity.dy;
+        moon.position.z += moon.velocity.dz;
+      }
+      moons = newMoons.ToArray();
+    }
+
+    long totalEnergy = 0;
+
+    foreach (var moon in moons) {
+      var potential = Math.Abs(moon.position.x) + Math.Abs(moon.position.y) + Math.Abs(moon.position.z);
+      var kinetic = Math.Abs(moon.velocity.dx) + Math.Abs(moon.velocity.dy) + Math.Abs(moon.velocity.dz);
+      totalEnergy += potential * kinetic;
+    }
+
+    return totalEnergy;
+  }
+
+  private static long PartTwo(Moon[] moons) {
+    List<Moon> origMoons = [];
+    Array.ForEach(moons, x => origMoons.Add(x.Copy()));
+
+    var step = 0;
+    bool foundX = false, foundY = false, foundZ = false;
+    long frequencyX = 0, frequencyY = 0, frequencyZ = 0;
+
+    while (true) {
+      step++;
+      List<Moon> newMoons = [];
+      foreach (var moon in moons) {
+        newMoons.Add(moon.Copy());
+      }
+
+      for (var i = 0; i < moons.Length; i++) {
+        for (var j = 0; j < moons.Length; j++) {
+          if (i == j) continue;
+          newMoons[i].velocity.dx -= moons[i].position.x.CompareTo(moons[j].position.x);
+          newMoons[i].velocity.dy -= moons[i].position.y.CompareTo(moons[j].position.y);
+          newMoons[i].velocity.dz -= moons[i].position.z.CompareTo(moons[j].position.z);
+        }
+      }
+      foreach (var moon in newMoons) {
+        moon.position.x += moon.velocity.dx;
+        moon.position.y += moon.velocity.dy;
+        moon.position.z += moon.velocity.dz;
+      }
+
+      moons = newMoons.ToArray();
+      bool sameX = true, sameY = true, sameZ = true;
+      for (var i = 0; i < moons.Length; i++) {
+        if (!foundX && sameX && !moons[i].IsMatchInX(origMoons[i])) {
+          sameX = false;
+        }
+        if (!foundY && sameY && !moons[i].IsMatchInY(origMoons[i])) {
+          sameY = false;
+        }
+        if (!foundZ && sameZ && !moons[i].IsMatchInZ(origMoons[i])) {
+          sameZ = false;
+        }
+      }
+      if (!foundX && sameX) {
+        foundX = true;
+        frequencyX = step;
+      }
+      if (!foundY && sameY) {
+        foundY = true;
+        frequencyY = step;
+      }
+      if (!foundZ && sameZ) {
+        foundZ = true;
+        frequencyZ = step;
+      }
+      if (foundX && foundY && foundZ) {
+        break;
+      }
+    }
+
+    var lcd = LargestCommonDivisor(frequencyX, frequencyY);
+    var frequencyXY = frequencyX * frequencyY / lcd;
+    lcd = LargestCommonDivisor(frequencyXY, frequencyZ);
+
+    return frequencyXY * frequencyZ / lcd;
+  }
+
+  private static long LargestCommonDivisor(long a, long b) {
+    while (b != 0) {
+      long temp = b;
+      b = a % b;
+      a = temp;
+    }
+    return Math.Abs(a);
+  }
+
+  private static void PrintStatus(Moon[] moons, int steps) {
+    Console.WriteLine($"After {steps} steps:");
+    foreach (var moon in moons) {
+      Console.WriteLine(moon.ToString());
+    }
+  }
+
+  private class Moon(int x, int y, int z) {
+    internal (int x, int y, int z) position = (x, y, z);
+    internal (int dx, int dy, int dz) velocity = (0, 0, 0);
+
+    internal bool IsMatchInX(Moon other) {
+      return position.x == other.position.x && velocity.dx == other.velocity.dx;
+    }
+    internal bool IsMatchInY(Moon other) {
+      return position.y == other.position.y && velocity.dy == other.velocity.dy;
+    }
+    internal bool IsMatchInZ(Moon other) {
+      return position.z == other.position.z && velocity.dz == other.velocity.dz;
+    }
+
+
+    internal Moon Copy() {
+      var newMoon = new Moon(position.x, position.y, position.z);
+      newMoon.velocity.dx = velocity.dx;
+      newMoon.velocity.dy = velocity.dy;
+      newMoon.velocity.dz = velocity.dz;
+      return newMoon;
+    }
+    public override string ToString() {
+      return $"position: {position}, velocity:{velocity}";
+    }
+  }
+
+  private static Moon[] GetData(string filePath) {
+    if (string.IsNullOrWhiteSpace(filePath)) {
+      filePath = "sample.txt";
+    }
+
+    using var streamReader = new StreamReader(filePath);
+    var data = streamReader.ReadToEnd().Split('\n', StringSplitOptions.RemoveEmptyEntries);
+
+    List<Moon> moons = [];
+    foreach (var line in data) {
+      var m = MyRegex.InputRegex().Matches(line);
+      if (m.Count != 3) {
+        throw new ApplicationException($"Should find 3 input values. Fount:{m.Count}");
+      }
+      var x = int.Parse(m[0].Value);
+      var y = int.Parse(m[1].Value);
+      var z = int.Parse(m[2].Value);
+      moons.Add(new Moon(x, y, z));
+    }
+
+    return moons.ToArray();
+  }
+
+  private static void PrintResult(string partNo, string result, Stopwatch sw) {
+    sw.Stop();
+    Console.WriteLine($"Part {partNo} Result: {result} in {sw.Elapsed.TotalMilliseconds}ms");
+    sw.Restart();
+  }
+
+  internal static partial class MyRegex {
+    [GeneratedRegex(@"(-?\d+)", RegexOptions.Singleline)]
+    public static partial Regex InputRegex();
+  }
+}
---
+++ Day12/readme.md
@@ -0,0 +1,17 @@
+## Day 12: The N-Body Problem ##
+https://adventofcode.com/2019/day/12
+
+File: sample.txt
+
+Part 1 Result: 179 in 2.0768ms\
+Part 2 Result: 2772 in 1.0154ms
+
+File: sample2.txt
+
+Part 1 Result: 1940 in 0.2369ms\
+Part 2 Result: 4686774924 in 8.9626ms
+
+File: input.txt
+
+Part 1 Result: 7471 in 3.1468ms\
+Part 2 Result: 376243355967784 in 110.665ms
