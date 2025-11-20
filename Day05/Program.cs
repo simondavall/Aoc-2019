5a626dbc2d1573dfb253cc7ec8476481f8eb0d7d
Simon Da Vall <simon.davall@gmail.com>
2025-11-20 19:59:52 +0000

    Day05

--- Day05/Program.cs
+++ Day05/Program.cs
@@ -4,21 +4,18 @@ using Spacecraft;
 
 namespace Day05;
 
-internal static class Program
-{
+internal static class Program {
   private const long ExpectedPartOne = 10987514;
   private const long ExpectedPartTwo = 14195011;
 
-  public static int Main(string[] args)
-  {
+  public static int Main(string[] args) {
     Console.WriteLine("\n## Day 5: Sunny with a Chance of Asteroids ##");
     Console.WriteLine("https://adventofcode.com/2019/day/5");
- 
+
     long resultPartOne = -1;
     long resultPartTwo = -1;
 
-    foreach (var filePath in args)
-    {
+    foreach (var filePath in args) {
       Console.WriteLine($"\nFile: {filePath}\n");
       long[] program = GetData(filePath);
       var stopwatch = Stopwatch.StartNew();
@@ -34,30 +31,24 @@ internal static class Program
     return resultPartOne == ExpectedPartOne && resultPartTwo == ExpectedPartTwo ? 0 : 1;
   }
 
-  private static long PartOne(long[] program)
-  {
+  private static long PartOne(long[] program) {
     var computer = new IntcodeComputer(program);
-    while (!computer.IsHalted)
-    {
-      if (computer.IsAwaitingInput)
-      {
+    while (!computer.IsHalted) {
+      if (computer.IsAwaitingInput) {
         computer.SetInput(1);
       }
       computer.Execute();
     }
 
     Console.WriteLine($"Full output: {computer.FullOutput.Print()}");
-    
+
     return computer.GetLastOutput;
   }
 
-  private static long PartTwo(long[] program)
-  {
+  private static long PartTwo(long[] program) {
     var computer = new IntcodeComputer(program);
-    while (!computer.IsHalted)
-    {
-      if (computer.IsAwaitingInput)
-      {
+    while (!computer.IsHalted) {
+      if (computer.IsAwaitingInput) {
         computer.SetInput(5);
       }
       computer.Execute();
@@ -66,12 +57,7 @@ internal static class Program
     return computer.GetLastOutput;
   }
 
-  private static long[] GetData(string filePath)
-  {
-    if (string.IsNullOrWhiteSpace(filePath)){
-      filePath = "sample.txt";
-    }
-
+  private static long[] GetData(string filePath) {
     using var streamReader = new StreamReader(filePath);
     long[] data = streamReader
       .ReadToEnd()
@@ -81,8 +67,7 @@ internal static class Program
     return data;
   }
 
-  private static void PrintResult(string partNo, string result, Stopwatch sw)
-  {
+  private static void PrintResult(string partNo, string result, Stopwatch sw) {
     sw.Stop();
     Console.WriteLine($"Part {partNo} Result: {result} in {sw.Elapsed.TotalMilliseconds}ms");
     sw.Restart();
--- Day05/readme.md
+++ Day05/readme.md
@@ -4,12 +4,12 @@ https://adventofcode.com/2019/day/5
 
 File: sample.txt
 
-Full output: [999]
-Part 1 Result: 999 in 5.375ms
+Full output: [999]\
+Part 1 Result: 999 in 5.375ms\
 Part 2 Result: 999 in 0.1341ms
 
 File: input.txt
 
-Full output: [0, 0, 0, 0, 0, 0, 0, 0, 0, 10987514]
-Part 1 Result: 10987514 in 1.0466ms
+Full output: [0, 0, 0, 0, 0, 0, 0, 0, 0, 10987514]\
+Part 1 Result: 10987514 in 1.0466ms\
 Part 2 Result: 14195011 in 0.0636ms
