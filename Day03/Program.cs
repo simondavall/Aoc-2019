9d0519700b52a7889e51efe056fe2f6e1549266b
Simon Da Vall <simon.davall@gmail.com>
2025-11-20 19:55:03 +0000

    Day03

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -2,23 +2,24 @@
 
 namespace Day03;
 
-internal static class Program
-{
+internal static class Program {
   private const long ExpectedPartOne = 1431;
   private const long ExpectedPartTwo = 48012;
 
-  private static readonly Dictionary<char, (int dr, int dc)> Directions = new() { { 'U', (-1, 0) }, { 'R', (0, 1) }, { 'D', (1, 0) }, { 'L', (0, -1) } };
+  private static readonly Dictionary<char, (int dr, int dc)> Directions = new()
+  {{ 'U', (-1, 0) },
+    { 'R', (0, 1) },
+    { 'D', (1, 0) },
+    { 'L', (0, -1) } };
 
-  public static int Main(string[] args)
-  {
+  public static int Main(string[] args) {
     Console.WriteLine("\n## Day 3: Crossed Wires ##");
     Console.WriteLine("https://adventofcode.com/2019/day/3");
- 
+
     long resultPartOne = -1;
     long resultPartTwo = -1;
 
-    foreach (var filePath in args)
-    {
+    foreach (var filePath in args) {
       Console.WriteLine($"\nFile: {filePath}\n");
       (char dir, int val)[][] input = GetData(filePath);
       var stopwatch = Stopwatch.StartNew();
@@ -33,19 +34,16 @@ internal static class Program
     return resultPartOne == ExpectedPartOne && resultPartTwo == ExpectedPartTwo ? 0 : 1;
   }
 
-  private static long PartOne((char, int)[][] wires)
-  {
+  private static long PartOne((char, int)[][] wires) {
     long minDist = int.MaxValue;
 
     var visited = new HashSet<(int, int)>();
 
     // first wire path
     int r = 0, c = 0;
-    foreach (var (dir, val) in wires[0])
-    {
+    foreach (var (dir, val) in wires[0]) {
       var (dr, dc) = Directions[dir];
-      for (var i = 0; i < val; i++)
-      {
+      for (var i = 0; i < val; i++) {
         r += dr;
         c += dc;
         visited.Add((r, c));
@@ -55,15 +53,12 @@ internal static class Program
     // compare second wire path with first
     r = 0;
     c = 0;
-    foreach (var (dir, val) in wires[1])
-    {
+    foreach (var (dir, val) in wires[1]) {
       var (dr, dc) = Directions[dir];
-      for (var i = 0; i < val; i++)
-      {
+      for (var i = 0; i < val; i++) {
         r += dr;
         c += dc;
-        if (visited.Contains((r, c)))
-        {
+        if (visited.Contains((r, c))) {
           var dist = Math.Abs(r) + Math.Abs(c);
           if (minDist > dist)
             minDist = dist;
@@ -74,23 +69,20 @@ internal static class Program
     return minDist;
   }
 
-  private static long PartTwo((char, int)[][] wires)
-  {
+  private static long PartTwo((char, int)[][] wires) {
     long minDist = int.MaxValue;
 
     var visited = new Dictionary<(int, int), int>();
 
     // first wire path
     int r = 0, c = 0, steps = 0;
-    foreach (var (dir, val) in wires[0])
-    {
+    foreach (var (dir, val) in wires[0]) {
       var (dr, dc) = Directions[dir];
-      for (var i = 0; i < val; i++)
-      {
+      for (var i = 0; i < val; i++) {
         steps++;
         r += dr;
         c += dc;
-        if (!visited.ContainsKey((r,c)))
+        if (!visited.ContainsKey((r, c)))
           visited.Add((r, c), steps);
       }
     }
@@ -99,17 +91,14 @@ internal static class Program
     r = 0;
     c = 0;
     steps = 0;
-    foreach (var (dir, val) in wires[1])
-    {
+    foreach (var (dir, val) in wires[1]) {
       var (dr, dc) = Directions[dir];
-      for (var i = 0; i < val; i++)
-      {
+      for (var i = 0; i < val; i++) {
         steps++;
         r += dr;
         c += dc;
-        if (visited.ContainsKey((r, c)))
-        {
-          var dist = steps + visited[(r,c)];
+        if (visited.ContainsKey((r, c))) {
+          var dist = steps + visited[(r, c)];
           if (minDist > dist)
             minDist = dist;
         }
@@ -119,12 +108,7 @@ internal static class Program
     return minDist;
   }
 
-  private static (char dir, int val)[][] GetData(string filePath)
-  {
-    if (string.IsNullOrWhiteSpace(filePath)){
-      filePath = "sample.txt";
-    }
-
+  private static (char dir, int val)[][] GetData(string filePath) {
     using var streamReader = new StreamReader(filePath);
     var data = streamReader.ReadToEnd().Split('\n', StringSplitOptions.RemoveEmptyEntries);
 
@@ -137,8 +121,7 @@ internal static class Program
     return wires;
   }
 
-  private static void PrintResult(string partNo, string result, Stopwatch sw)
-  {
+  private static void PrintResult(string partNo, string result, Stopwatch sw) {
     sw.Stop();
     Console.WriteLine($"Part {partNo} Result: {result} in {sw.Elapsed.TotalMilliseconds}ms");
     sw.Restart();
--- Day03/readme.md
+++ Day03/readme.md
@@ -4,10 +4,10 @@ https://adventofcode.com/2019/day/3
 
 File: sample.txt
 
-Part 1 Result: 159 in 4.5558ms
+Part 1 Result: 159 in 4.5558ms\
 Part 2 Result: 610 in 2.1157ms
 
 File: input.txt
 
-Part 1 Result: 1431 in 19.7208ms
+Part 1 Result: 1431 in 19.7208ms\
 Part 2 Result: 48012 in 22.0976ms
