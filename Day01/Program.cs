16efe842145e2820a1b40e210ced399ee5512798
Simon Da Vall <simon.davall@gmail.com>
2025-11-20 21:42:43 +0000

    Day01

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,33 +1,11 @@
-﻿using System.Diagnostics;
-using AocHelper;
+﻿namespace Day01;
 
-namespace Day01;
+internal static partial class Program {
+  private const string Title = "\n## Day 1: The Tyranny of the Rocket Equation ##";
+  private const string AdventOfCode = "https://adventofcode.com/2019/day/1";
 
-internal static class Program {
-  private const long ExpectedPartOne = 3226822;
+ private const long ExpectedPartOne = 3226822;
   private const long ExpectedPartTwo = 4837367;
-
-  public static int Main(string[] args) {
-    Console.WriteLine("\n## Day 1: The Tyranny of the Rocket Equation ##");
-    Console.WriteLine("https://adventofcode.com/2019/day/1");
-
-    long resultPartOne = -1;
-    long resultPartTwo = -1;
-
-    foreach (var filePath in args) {
-      Console.WriteLine($"\nFile: {filePath}\n");
-      int[] input = GetData(filePath).ToIntArray();
-      var stopwatch = Stopwatch.StartNew();
-
-      resultPartOne = PartOne(input);
-      PrintResult("1", resultPartOne.ToString(), stopwatch);
-
-      resultPartTwo = PartTwo(input);
-      PrintResult("2", resultPartTwo.ToString(), stopwatch);
-    }
-
-    return resultPartOne == ExpectedPartOne && resultPartTwo == ExpectedPartTwo ? 0 : 1;
-  }
 
   private static long PartOne(int[] modules) {
     long tally = 0;
@@ -55,17 +33,5 @@ internal static class Program {
     }
 
     return tally;
-  }
-
-  private static string[] GetData(string filePath) {
-    using var streamReader = new StreamReader(filePath);
-    var data = streamReader.ReadToEnd().Split('\n', StringSplitOptions.RemoveEmptyEntries);
-    return data;
-  }
-
-  private static void PrintResult(string partNo, string result, Stopwatch sw) {
-    sw.Stop();
-    Console.WriteLine($"Part {partNo} Result: {result} in {sw.Elapsed.TotalMilliseconds}ms");
-    sw.Restart();
   }
 }
